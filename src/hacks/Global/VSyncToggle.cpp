7c53386ca5ff59a12f5c139e9a4bebaf7af2215d
gtxx9903 <gtxripmxtlol@gmail.com>
2025-08-16 11:11:09 +0300

    add v-sync toggle

---
+++ src/hacks/Global/VSyncToggle.cpp
@@ -0,0 +1,26 @@
+#include <modules/config/config.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/gui/components/toggle.hpp>
+#include <modules/hack/hack.hpp>
+
+namespace eclipse::hacks::Global {
+    class $hack(VSyncToggle) {
+        void init() override {
+            auto vsync = GameManager::sharedState()->getGameVariable("0030");
+             
+            auto tab = gui::MenuTab::find("tab.global");
+            tab->addToggle("global.vsync-toggle", "global.v-sync.toggle")
+               ->handleKeybinds()
+               ->setDescription()
+               ->callback([](bool v) {
+                   GameManager::sharedState()->setGameVariable("0030", v);
+               })->disableSaving();
+
+            config::set("global.v-sync.toggle", vsync);
+        }
+      
+        [[nodiscard]] const char* getId() const override { return "V-Sync Toggle"; }
+    }
+
+    REGISTER_HACK(VSyncToggle)
+}
